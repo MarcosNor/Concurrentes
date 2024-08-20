cc1a3c96ac4a66d9a8f63be8fae176c667e501fc
MarcosNor <noriegamarcos44@ciencias.unam.mx>
2024-08-19 23:06:47 -0600

    procesadores a hilos

--- Practica01/NumHilos.java
+++ Practica01/NumHilos.java
@@ -1,6 +1,6 @@
 public class NumHilos {
     public static void main(String[] args) {
-        System.out.println("Número de procesadores disponibles: " + Runtime.getRuntime().availableProcessors());
+        System.out.println("Número de hilos disponibles: " + Runtime.getRuntime().availableProcessors());
     }
 }
 
