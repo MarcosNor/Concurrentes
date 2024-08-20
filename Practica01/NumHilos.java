e5fb2d59df57a9eb4c28caaca2edc6be57f83676
MarcosNor <noriegamarcos44@ciencias.unam.mx>
2024-08-19 22:31:43 -0600

    Archivo de ejercicio 2

---
+++ Practica01/NumHilos.java
@@ -0,0 +1,6 @@
+public class NumHilos {
+    public static void main(String[] args) {
+        System.out.println("Número de procesadores disponibles: " + Runtime.getRuntime().availableProcessors());
+    }
+}
+
